83ca6c5abb20f90cd9ec941e94c028ed60ca9cfa
zeroTn <805662439@qq.com>
2017-02-23 11:23:46 +0800

    no commit message

--- cjx_produceFigure.py
+++ cjx_produceFigure.py
@@ -41,7 +41,7 @@ def getGroundTruthList(groundTruthDF,shop_id):
     return countseries[length-15:length-1]
 
 
-def produceOneFigure(groundTruthDF,predictedDF,shop_id,title,copy_path,view_ax):
+def produceOneFigure(groundTruthDF,predictedDF,shop_id,title,copy_path):
     '''
      绘制预测和真实值两者的序列
     :param groundTruthDF: 现实数据的真实值数据框
@@ -52,7 +52,8 @@ def produceOneFigure(groundTruthDF,predictedDF,shop_id,title,copy_path,view_ax):
     :return:
     '''
 
-
+    fig=plt.figure(figsize=(15,8))
+    view_ax = fig.add_subplot(1, 1, 1)
     plt.xlabel('date')
     plt.ylabel('view_counts')
     xticklis=range(1,15)
@@ -67,11 +68,12 @@ def produceOneFigure(groundTruthDF,predictedDF,shop_id,title,copy_path,view_ax):
     view_ax.plot(xticklis, predictedList, 'c--', marker='.', linewidth=0.5);
     plt.savefig(copy_path)
     view_ax.clear()
+    plt.close()
 
 
 
 
-def produceOneGapFigure(groundTruthDF,predictedDF,shop_id,title,copy_path,view_ax):
+def produceOneGapFigure(groundTruthDF,predictedDF,shop_id,title,copy_path):
     '''
      绘制差值序列
     :param groundTruthDF: 现实数据的真实值数据框
@@ -82,7 +84,8 @@ def produceOneGapFigure(groundTruthDF,predictedDF,shop_id,title,copy_path,view_a
     :return:             
     '''
 
-
+    fig = plt.figure(figsize=(15, 8))
+    view_ax = fig.add_subplot(1, 1, 1)
     plt.xlabel('date')
     plt.ylabel('view_counts')
     xticklis=range(1,15)
@@ -98,18 +101,19 @@ def produceOneGapFigure(groundTruthDF,predictedDF,shop_id,title,copy_path,view_a
     plt.savefig(copy_path)
     view_ax.clear()
 
-def produceOneScoreFigure(groundTruthDF,predictedDF,shop_id,title,copy_path,view_ax):
+def produceOneScoreFigure(groundTruthDF,predictedDF,shop_id,title,copy_path):
     '''
-     绘制分数序列
+    绘制分数序列
     :param groundTruthDF: 现实数据的真实值数据框
     :param predictedDF:   预测出来的数据的数据集框
     :param shop_id:       商店id
     :param title:         figure的标题
-    :param copy_path:     figure保存路径
+    :param copy_path:     figure保存路径  ..../../../xx.png
     :return:
     '''
 
-
+    fig = plt.figure(figsize=(15, 8))
+    view_ax = fig.add_subplot(1, 1, 1)
     plt.xlabel('date')
     plt.ylabel('view_counts')
     xticklis=range(1,15)
@@ -131,7 +135,7 @@ def produceOneScoreFigure(groundTruthDF,predictedDF,shop_id,title,copy_path,view
     view_ax.clear()
 
 
-def GenerateAllContrastFig(groundTruthDF,predictedDF,shopDF,figtype):
+def GenerateAllContrastFig(groundTruthDF,predictedDF,shopDF,savepath,figtype):
     '''
 
     :param groundTruthDF:
@@ -140,8 +144,11 @@ def GenerateAllContrastFig(groundTruthDF,predictedDF,shopDF,figtype):
     :param figtype: 0:绘制两者序列  1:gap=Tg-Tp(差值序列) 2:gap=(Tg-Tp)/(Tg+Tp)(分数序列)
      :return:
     '''
+    import  os
+    if os.path.exists(savepath)==False:
+        os.mkdir(savepath)
     ID_list=predict_data['shop_id'].unique()
-    fig=plt.figure(figsize=(15,8))
+    fig = plt.figure(figsize=(15, 8))
     view_ax = fig.add_subplot(1, 1, 1)
     for id_item in ID_list:
         # if id_item<499:
@@ -153,19 +160,22 @@ def GenerateAllContrastFig(groundTruthDF,predictedDF,shopDF,figtype):
                 + str(_cur_shop_info.comment_cnt.values[0]) + 'cate1_name:' + str(_cur_shop_info.cate1_name.values[0]) + '\ncate2_name:' \
                + str(_cur_shop_info.cate2_name.values[0]) + 'cate3_name:' + str(_cur_shop_info.cate3_name.values[0])
         # print 'title:',title
-        if str(_cur_shop_info.cate1_name.values[0]) == '美食':
-             copy_path = 'contrast_figure\\food_score_f4\\'+str(id_item)+'_contrastscore.png';
-        else:
-            if str(_cur_shop_info.cate1_name.values[0]) == '超市便利店':
-                copy_path = 'contrast_figure\\market_score_f4\\'+str(id_item)+'_contrastscore.png'
-            else:
-                copy_path = 'contrast_figure\\other_score_f4\\'+str(id_item)+'_contrastscore.png'
+        catename=_cur_shop_info.cate1_name.values[0]
+
+        if type(catename)==float:
+            catename=='nan'
+        # 将中文按utf-8格式解码
+        catename=catename.decode('utf-8')
+        flodername=savepath +catename +'/'
+        if os.path.exists(flodername) == False:
+            os.mkdir(flodername)
+        figure_path=flodername+str(id_item)+'_contrastscore.png'
         if figtype==0:
-           produceOneFigure(groundTruthDF,predictedDF,id_item,title,copy_path,view_ax)
+           produceOneFigure(groundTruthDF,predictedDF,id_item,title,figure_path)
         if figtype == 1:
-            produceOneGapFigure(groundTruthDF, predictedDF, id_item, title, copy_path, view_ax)
+            produceOneGapFigure(groundTruthDF, predictedDF, id_item, title, figure_path)
         if figtype == 2:
-            produceOneScoreFigure(groundTruthDF, predictedDF, id_item, title, copy_path, view_ax)
+            produceOneScoreFigure(groundTruthDF, predictedDF, id_item, title, figure_path)
 
 if __name__=='__main__':
     set_ch()
@@ -173,12 +183,16 @@ if __name__=='__main__':
 
     namelist = ['shop_id', 'day1', 'day2', 'day3', 'day4', 'day5', 'day6', 'day7', 'day8',
                 'day9', 'day10', 'day11', 'day12', 'day13', 'day14']
-    predict_data = pd.read_csv('result\\result_revise_f4_train.csv', names=namelist)
+    predict_data = pd.read_csv('result/result_revise_f4_train.csv', names=namelist)
     train_data=pd.read_csv(param.payAfterGroupingAndRevision_path)
 
     shop_path='data/shop_info.txt'
 
     shop_info=pd.read_csv(shop_path,names=['shop_id','city_name','location_id','per_pay',
 										   'score','comment_cnt','shop_level','cate1_name','cate2_name','cate3_name'])
-
-    GenerateAllContrastFig(train_data,predict_data,shop_info,2)
+    savepath='figures_contrast/'
+    # 使用方式：仿照被注释了的调用
+    GenerateAllContrastFig(train_data,predict_data,shop_info,savepath,0)
+    # produceOneFigure(train_data,predict_data,3,'..','3_c.png')
+    # produceOneGapFigure(train_data,predict_data,3,'..','3_G.png')
+    # produceOneScoreFigure(train_data,predict_data,3,'..','3_S.png')
